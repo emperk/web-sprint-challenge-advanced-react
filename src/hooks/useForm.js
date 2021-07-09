f851c328157e5cfa8b0a8f85101fcb5f82959165
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-07-09 13:12:15 -0700

    completed assignment

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,13 +2,17 @@
 import { useState } from "react";
 
 const useForm = (initialValues) => {
-
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, setValues] = useState(initialValues);
   const handleChanges = (event) => {
     console.log("the event:", event);
     setValues({...values, [event.target.name]: event.target.value});
   };
-  return [values, handleChanges]
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowSuccessMessage(true);
+  }
+  return [values, handleChanges, handleSubmit, showSuccessMessage];
 }
 
 export default useForm;
